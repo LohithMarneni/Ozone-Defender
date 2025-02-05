c7ad6e7c42510bf69858e3013bf04df3d016989c
LohithMarneni <lohith.marneni@gmail.com>
2025-02-05 09:41:50 +0530

    Final COmmit

--- Backend/app.js
+++ Backend/app.js
@@ -5,11 +5,11 @@ import { errorMiddleWare } from "./middleware/error.js";
 const app = express();
 app.use(express.json());
 app.use(cors(
-    {
-        origin:[process.env.FRONTEND_URL],
-        methods:["GET","POST","PUT","DELETE","PATCH"],
-        credentials:true,
-    }
+    // {
+    //     origin:[process.env.FRONTEND_URL],
+    //     methods:["GET","POST","PUT","DELETE","PATCH"],
+    //     credentials:true,
+    // }
 ))
 app.use('/api/v1',donationRouter);
 app.use(errorMiddleWare);
